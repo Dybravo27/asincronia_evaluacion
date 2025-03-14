68f5519b77aa2300b243bc1b92a841387ecbac43
Dylan Bravo <dylanslebyng@gmail.com>
2025-03-14 11:33:07 -0500

    ejercicio 4 terminado y ejercicio 1 comentarios arreglados

--- ejercicio 4/ejercicio4.js
+++ ejercicio 4/ejercicio4.js
@@ -1,6 +1,5 @@
-// se importa la funcion getTodos y getUsuarios que hacen la peticion
-// para traer los datos del usuario y la lista de tareas de cada uno
-import { getTodos } from "../ejercicio 5/modulos/todos.js";
+// se importa la funcion getUsuarios que hace la peticion que trae a
+// los usuarios 
 import { getUsuarios } from "../ejercicio 5/modulos/usuarios.js";
 // se declara una variable de tipo const se asigna la url de la api
 const URL = "https://jsonplaceholder.typicode.com";
@@ -10,16 +9,14 @@ const manejardatos = async () => {
     // se declara la variable usuarios donde se almacena los usuarios traidos en la peticion
     const usuarios = await getUsuarios(URL);
     // se usa el Promise.all para ejecutar varias promesas, en este caso una por cada usuario
-    // y con el .map se recorre todo el arreglo y si se cumple la promesa se retorna el usuario con la lista de tareas
+    // y con el .map se recorre todo el arreglo y si se cumple la promesa
+    // nos muestra el nombre del usuario con el telefono
     return await Promise.all(usuarios.map(async (usuario) => {
-      const tareas = await getTodos(URL, usuario); // se obtienen los posts de cada usuario
-      // se usa el Promise.all para ejecutar varias promesas, en este caso una por cada usuario
-      // y con el .map se recorre todo el arreglo y si se cumple la promesa se retorna el usuario con la lista de tareas
-      const listaTareas = await Promise.all(tareas.map(async (tarea) => {
-        return { ...tarea }; // se retorna la lista de tareas
-      }));
-      // retorna el usuario con la lista de tareas
-      return { ...usuario, listaTareas };
+      // se retorna el arreglo con el nombre y el telefono
+      return {
+        "nombre" : usuario.name,
+        "telefono": usuario.phone
+      };
     }));
   } catch (error) {
     throw new Error("Ocurrio una falla : " + error);
