adb325fa6c23755db5e199f7a18519b2ef9abc80
Dylan Bravo <dylanslebyng@gmail.com>
2025-03-14 09:55:22 -0500

    ejercicio 1 y 5 arreglo comentarios,se agrega try catch

--- ejercicio 1/ejercicio1.js
+++ ejercicio 1/ejercicio1.js
@@ -1,9 +1,11 @@
-// se importa la funcion getTodos 
+// se importa la funcion getTodos y getUsuarios para usar los metodos los cuales traen 
+// los usuarios que tienen la lista de tareas
 import { getTodos } from "../ejercicio 5/modulos/todos.js";
 import { getUsuarios } from "../ejercicio 5/modulos/usuarios.js";
 // se declara una variable de tipo const se asigna la url de la api
 const URL = "https://jsonplaceholder.typicode.com";
 const manejardatos = async () => {
+  // se manejan los errores con el try catch en caso de haber algun error en la peticion
   try {
     // se declara la variable usuarios donde se almacena los usuarios traidos en la peticion
     const usuarios = await getUsuarios(URL);
@@ -11,12 +13,13 @@ const manejardatos = async () => {
     // y con el .map se recorre todo el arreglo y si se cumple la promesa se retorna el usuario con la lista de tareas
     return await Promise.all(usuarios.map(async (usuario) => {
       const tareas = await getTodos(URL, usuario); // se obtienen los posts de cada usuario
-      // se obtienen la lista de tareas de cada post usando Promise.all
+      // se usa el Promise.all para ejecutar varias promesas, en este caso una por cada usuario
+      // y con el .map se recorre todo el arreglo y si se cumple la promesa se retorna el usuario con la lista de tareas
       const listaTareas = await Promise.all(tareas.map(async (tarea) => {
-        return { ...tarea}; // retorna el usuario con sus posts y comentarios
+        return { ...tarea }; // se retorna la lista de tareas
       }));
       // retorna el usuario con la lista de tareas
-      return { ...usuario, listaTareas};
+      return { ...usuario, listaTareas };
     }));
   } catch (error) {
     throw new Error("Ocurrio una falla : " + error);
--- ejercicio 5/app.js
+++ ejercicio 5/app.js
@@ -1,30 +1,34 @@
-// se importan las funciones desde el index.js
+// se importan las funciones donde se realizan las solicitudes de usuarios, post,comentarios,albumes y fotos desde el index.js
 import { getUsuarios, getPost, getCommets,getAlbumes,getFotos } from "./modulos/index.js";
 // se declara una variable de tipo const se asigna la url de la api
 export const URL = "https://jsonplaceholder.typicode.com";
 const manejardatos = async () => {
-  // se declara la variable usuarios donde se almacena los usuarios traidos en la peticion
-  const usuarios = await getUsuarios(URL);
-  // se usa el Promise.all para ejecutar varias promesas, en este caso una por cada usuario
-  // y con el .map se recorre todo el arreglo y si se cumple la promesa se retorna el usuario con los posts, los comentarios y los albums con las fotos
-  return await Promise.all(usuarios.map(async (usuario) => {
-    const posts = await getPost(URL, usuario); // se obtienen los posts de cada usuario
-    // se declara la variable albums donde se guarda el arreglo con los albums que tiene cada usuario
-    const albums = await getAlbumes(URL, usuario.id);
-    // se obtienen los albums de cada usuario
-    // se obtiene todas las fotos de cada album con el .map recorremos cada elemento del arreglo
-    const albumFotos = await Promise.all(albums.map(async (album) => {
-      const fotos = await getFotos(URL, album);// se obtiene la lista de fotos del album haciendo una peticion a la api
-      return { ...album, fotos } // retorna el album con sus fotos
-    }))
-    // se obtienen los comentarios de cada post con el .map recorremos cada elemento del arreglo
-    const comentPost = await Promise.all(posts.map(async (post) => {
-      const coments = await getCommets(URL, post); // se obtiene los comentarios del post haciendo una peticion a la api
-      return { ...post, coments }; // retorna el usuario con sus posts y comentarios
+  // se manejan los errores con el try catch en caso de haber algun error en las peticiones
+  try {
+    // se declara la variable usuarios donde se almacena los usuarios traidos en la peticion
+    const usuarios = await getUsuarios(URL);
+    // se usa el Promise.all para ejecutar varias promesas, en este caso una por cada usuario
+    // y con el .map se recorre todo el arreglo y si se cumple la promesa se retorna el usuario con los posts, los comentarios y los albums con las fotos
+    return await Promise.all(usuarios.map(async (usuario) => {
+      const posts = await getPost(URL, usuario); // se obtienen los posts de cada usuario
+      // se declara la variable albums donde se guarda el arreglo con los albums que tiene cada usuario
+      const albums = await getAlbumes(URL, usuario.id); // se obtienen los albums de cada usuario
+      // se obtiene todas las fotos de cada album con el .map recorremos cada elemento del arreglo
+      const albumFotos = await Promise.all(albums.map(async (album) => {
+        const fotos = await getFotos(URL, album);// se obtiene la lista de fotos del album haciendo una peticion a la api
+        return { ...album, fotos } // retorna el album con sus fotos
+      }))
+      // se obtienen los comentarios de cada post con el .map recorremos cada elemento del arreglo
+      const comentPost = await Promise.all(posts.map(async (post) => {
+        const coments = await getCommets(URL, post); // se obtiene los comentarios del post haciendo una peticion a la api
+        return { ...post, coments }; // retorna el usuario con sus posts y comentarios
+      }));
+      // retorna el usuario con los posts, comentarios y albums con fotos
+      return { ...usuario, comentPost, albumFotos };
     }));
-    // retorna el usuario con los posts, comentarios y albums con fotos
-    return { ...usuario, comentPost, albumFotos };
-  }));
+  } catch (error) {
+    throw new Error("Ups ocurrio una falla : " + error);
+  }
 };
 // se llama la funcion y se muestra el primer usuario con sus datos
 manejardatos().then((data) => {
